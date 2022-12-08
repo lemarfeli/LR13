756621d93285ab01b368ed6b42689e379207e8d1
feli <117822922+lemarfeli@users.noreply.github.com>
2022-12-08 12:51:44 +0300

    Add files via upload

---
+++ 13.6.pas
@@ -0,0 +1,17 @@
+﻿begin
+var f1, f2: text;
+assign(f1,'c:\2.txt');
+assign(f2,'c:\3.txt');
+reset(f1);
+rewrite(f2);
+var a: string;
+ while not eof(f1) do
+  begin
+   readln(f1,a);
+   if a<>'' then println(f2,a);
+  end;
+close(f1);
+close(f2);
+erase(f1);
+rename(f2,'c:\2.txt');
+end.
