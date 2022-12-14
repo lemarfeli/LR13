fae4b45467b440242e4c5cf7af174e9d374cb05d
feli <117822922+lemarfeli@users.noreply.github.com>
2022-12-15 00:01:36 +0300

    Add files via upload

---
+++ 13.7.pas
@@ -0,0 +1,18 @@
+﻿begin
+var f:text;
+var a, r, h:integer;
+assign(f,'c:\z3.in.txt');
+reset(f);
+readln(f,a);
+for var i:=1 to a do 
+ begin
+r:=0;
+for var j:=1 to i do if i mod j = 0 then r:=r+1;
+if r=5 then h:=h+i;
+ end;
+close(f);
+assign(f,'c:\z3.out.txt');
+rewrite(f);
+println(f,h);
+close(f);
+end.
